ee6b462fdeb1165e775b6a627c1a60669b7a551d
khil94 <zidru94@gmail.com>
2025-03-12 00:18:12 +0900

    섹션들 w-screen -> w-full

--- src/sections/contactSection.tsx
+++ src/sections/contactSection.tsx
@@ -1,6 +1,6 @@
 export default function ContactSection() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold">Welcome to my Portfolio</h1>
     </div>
   );
