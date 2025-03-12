73a8b3cfaa3703f12394fe23f1b3e2be8da3bc02
khil94 <zidru94@gmail.com>
2025-03-12 23:34:48 +0900

    titleSection이 화면 전체를 차지하게 수정

--- src/sections/titleSection.tsx
+++ src/sections/titleSection.tsx
@@ -2,7 +2,7 @@ import { TITLE_PRODUCTS } from "../constants/title.ts";
 
 export default function TitleSection() {
   return (
-    <div className="w-full flex flex-col justify-center gap-10 items-center">
+    <div className="w-full h-screen flex flex-col justify-center gap-10 items-center">
       <div className="w-fit h-[30vh]  flex flex-col text-left justify-around text-6xl font-bold p-10">
         {TITLE_PRODUCTS.main_title.map((v, i) => {
           return <p key={`main_title_${i}`}>{v}</p>;
