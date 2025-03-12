b33366397d26f922ff68174d7d7548ff026873af
khil94 <zidru94@gmail.com>
2025-03-12 23:38:52 +0900

    구조 변경. sectionLayout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import "./App.css";
-import HomeSection from "./sections/homeSection.tsx";
-import AboutSection from "./sections/aboutSection.tsx";
-import ProjetsSection from "./sections/projetsSection.tsx";
-import ContactSection from "./sections/contactSection.tsx";
+
 import GlobalLayout from "./layouts/globalLayout.tsx";
 import TitleSection from "./sections/titleSection.tsx";
+import SectionLayout from "./layouts/sectionLayout.tsx";
 
 function App() {
   return (
     <>
       <GlobalLayout>
         <TitleSection />
-        <HomeSection />
-        <AboutSection />
-        <ProjetsSection />
-        <ContactSection />
+        <SectionLayout />
       </GlobalLayout>
     </>
   );
---
+++ src/layouts/sectionLayout.tsx
@@ -0,0 +1,21 @@
+import HomeSection from "../sections/homeSection.tsx";
+import AboutSection from "../sections/aboutSection.tsx";
+import ProjetsSection from "../sections/projetsSection.tsx";
+import ContactSection from "../sections/contactSection.tsx";
+import TOC from "../components/toc.tsx";
+
+export default function SectionLayout() {
+  return (
+    <div className={"w-full flex flex-row"}>
+      <div className={"w-[20%]"}>
+        <TOC />
+      </div>
+      <div className={"w-[80%]"}>
+        <HomeSection />
+        <AboutSection />
+        <ProjetsSection />
+        <ContactSection />
+      </div>
+    </div>
+  );
+}
