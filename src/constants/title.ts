fa0ec7aa15fa8afe80fd51f29535dcf1d5bf8727
khil94 <zidru94@gmail.com>
2025-03-12 19:28:11 +0900

    title section 구조변경
    constants의 상수를 참조하는 방식으로 수정

---
+++ src/constants/title.ts
@@ -0,0 +1,4 @@
+export const TITLE_PRODUCTS = {
+  main_title: ["프론트 엔드 개발자", "김효일", "포트폴리오"],
+  sub_title: "협업과 소통을 중요시 하는 개발자, 김효일 입니다.",
+} as const;
--- src/sections/titleSection.tsx
+++ src/sections/titleSection.tsx
@@ -1,10 +1,15 @@
+import { TITLE_PRODUCTS } from "../constants/title.ts";
+
 export default function TitleSection() {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full flex flex-col justify-center gap-10 items-center">
       <div className="w-fit h-[30vh]  flex flex-col text-left justify-around text-6xl font-bold p-10">
-        <p className="">프론트 엔드 개발자</p>
-        <p>김효일</p>
-        <p>포트폴리오</p>
+        {TITLE_PRODUCTS.main_title.map((v, i) => {
+          return <p key={`main_title_${i}`}>{v}</p>;
+        })}
+      </div>
+      <div className="">
+        <p>{TITLE_PRODUCTS.sub_title}</p>
       </div>
     </div>
   );
