d8abfddc64b6a7f9a5296eb7d42dd83f6c063783
khil94 <zidru94@gmail.com>
2025-03-20 19:29:08 +0900

    section wrapper 구조변경
    상하에서 좌우로 변경. 그에 따른 type값 추가

--- src/layouts/sectionWrapper.tsx
+++ src/layouts/sectionWrapper.tsx
@@ -3,13 +3,17 @@ import { ReactNode } from "react";
 interface Props {
   title: string;
   children: ReactNode;
+  type?: "left" | "right";
 }
 
-export default function SectionWrapper({ title, children }: Props) {
+export default function SectionWrapper({ type = "left", title, children }: Props) {
   return (
-    <div id={title} className={`w-full flex flex-col items-center justify-center`}>
+    <div
+      id={title}
+      className={`w-full flex ${type === "left" ? "flex-row" : "flex-row-reverse"} items-center justify-center `}
+    >
       <h1 className={`min-w-60 text-2xl font-bold text-center`}>{title}</h1>
-      <div className={`p-12`}>{children}</div>
+      <div className={`p-12 flex text-base/7 `}>{children}</div>
     </div>
   );
 }
