806de4d25190104b9fd258f800490d49a5a8be44
khil94 <zidru94@gmail.com>
2025-03-22 00:38:45 +0900

    projectSection 디자이닝
    개발중. 현재 메뉴/웹/깃 아이콘 연결 및 타이틀/설명/요약 이 표기되게 개발

--- src/components/projectComp.tsx
+++ src/components/projectComp.tsx
@@ -1,4 +1,7 @@
 import { Project } from "../constants/projects.ts";
+import Github from "../assets/github.svg?react";
+import Menu from "../assets/burger.svg?react";
+import Url from "../assets/web.svg?react";
 
 interface Prop {
   targetProject: Project;
@@ -6,8 +9,31 @@ interface Prop {
 
 export default function ProjectComp({ targetProject }: Prop) {
   return (
-    <div className={`min-w-full border-2 border-white rounded-2xl p-4 `}>
-      {targetProject.content}
+    <div
+      className={`min-w-full border-2 flex flex-col justify-between gap-4 text-center border-white rounded-2xl p-4 `}
+    >
+      <h2 className={"font-bold text-2xl"}>{targetProject.name}</h2>
+      <p>{targetProject.description}</p>
+      <div className={"p-4 text-left break-keep"}>
+        {targetProject.summary.map(v => {
+          return <p key={v}>{`· ${v}`}</p>;
+        })}
+      </div>
+      <div className={"flex flex-row justify-center gap-4"}>
+        <button>
+          <Menu width={40} height={40} stroke={"white"} fill={"white"} />
+        </button>
+        {targetProject.url && (
+          <a className={"w-fit"} href={targetProject.url} target={"_blank"}>
+            <Url width={40} height={40} stroke={"white"} fill={"white"} />
+          </a>
+        )}
+        {targetProject.git && (
+          <a href={targetProject.git} target={"_blank"}>
+            <Github width={40} height={40} stroke={"white"} fill={"white"} />
+          </a>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/projectCompWrapper.tsx
+++ src/components/projectCompWrapper.tsx
@@ -3,9 +3,8 @@ import ProjectComp from "./projectComp.tsx";
 import { useEffect, useRef, useState } from "react";
 
 export default function ProjectCompWrapper() {
-  const [idx, setIdx] = useState(1);
+  const [idx, setIdx] = useState(0);
   const totalLength = PROJECT_LIST.length;
-  const extendedList = [PROJECT_LIST[totalLength - 1], ...PROJECT_LIST, PROJECT_LIST[0]];
 
   const slidePrev = () => {
     setIdx(prev => (prev === 0 ? totalLength - 1 : prev - 1));
@@ -16,24 +15,26 @@ export default function ProjectCompWrapper() {
   };
 
   return (
-    <div className={`relative overflow-hidden`}>
-      <div
-        className={`
-        transition-transform duration-500 ease-in-out flex flex-row flex-nowrap`}
-        style={{ transform: `translateX(-${idx * 100}%)` }}
-      >
-        {extendedList.map((v, i) => {
-          return <ProjectComp key={`project-${v.name}`} targetProject={v} />;
-        })}
+    <div className={`relative`}>
+      <div className={`relative overflow-hidden`}>
+        <div
+          className={`
+            transition-transform duration-500 ease-in-out flex flex-row flex-nowrap`}
+          style={{ transform: `translateX(-${idx * 100}%)` }}
+        >
+          {PROJECT_LIST.map((v, i) => {
+            return <ProjectComp key={`project-${v.name}`} targetProject={v} />;
+          })}
+        </div>
       </div>
       <button
-        className="absolute top-1/2 left-2 p-2 bg-gray-800 text-white rounded-full"
+        className="absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-0 p-2 px-4 bg-gray-800 text-white rounded-full"
         onClick={slidePrev}
       >
         {"<"}
       </button>
       <button
-        className="absolute top-1/2 right-2 p-2 bg-gray-800 text-white rounded-full"
+        className="absolute top-1/2 -translate-y-1/2 right-0 translate-x-1/2 p-2 px-4 bg-gray-800 text-white rounded-full"
         onClick={slideNext}
       >
         {">"}
