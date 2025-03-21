3f5b5886cce203f4db60d2a018ebaffd4e1bcae1
khil94 <zidru94@gmail.com>
2025-03-22 03:50:46 +0900

    layout 의 max-width설정

--- src/layouts/globalLayout.tsx
+++ src/layouts/globalLayout.tsx
@@ -5,5 +5,9 @@ interface Prop {
 }
 
 export default function GlobalLayout({ children }: Prop) {
-  return <div className={" bg-main-light-black text-main-white w-full h-full"}>{children}</div>;
+  return (
+    <div className={"bg-main-light-black text-main-white flex justify-center"}>
+      <div className={" max-w-[1280px] w-full h-full flex flex-col"}>{children}</div>
+    </div>
+  );
 }
