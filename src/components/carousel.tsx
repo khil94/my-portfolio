d3ca6bbea20e63f98d0d2c85e26bbfb058aa2747
khil94 <zidru94@gmail.com>
2025-03-26 02:12:03 +0900

    캐러셀 컴포넌트로 분리

---
+++ src/components/carousel.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useState } from "react";
+
+interface Props<T> {
+  targetList: T[];
+  children: ReactNode;
+}
+
+export default function Carousel<T>({ targetList, children }: Props<T>) {
+  const [idx, setIdx] = useState(0);
+  const totalLength = targetList.length;
+
+  const slidePrev = () => {
+    setIdx(prev => (prev === 0 ? totalLength - 1 : prev - 1));
+  };
+
+  const slideNext = () => {
+    setIdx(prev => (prev === totalLength - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className={`relative text-base/8`}>
+      <div className={`relative overflow-hidden`}>
+        <div
+          className={`
+           flex flex-row transition-transform duration-500 ease-in-out`}
+          style={{ transform: `translateX(-${idx * 100}%)` }}
+        >
+          {children}
+        </div>
+      </div>
+      <button
+        className="absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-0 p-2 px-4 border-2 border-white bg-main-light-black text-white rounded-full"
+        onClick={slidePrev}
+      >
+        {"<"}
+      </button>
+      <button
+        className="absolute top-1/2 -translate-y-1/2 right-0 translate-x-1/2 p-2 px-4 border-2 border-white bg-main-light-black text-white rounded-full"
+        onClick={slideNext}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+}
--- src/components/projectCompWrapper.tsx
+++ src/components/projectCompWrapper.tsx
@@ -1,44 +1,13 @@
 import { PROJECT_LIST } from "../constants/projects.ts";
 import ProjectComp from "./projectComp.tsx";
-import { useState } from "react";
+import Carousel from "./carousel.tsx";
 
 export default function ProjectCompWrapper() {
-  const [idx, setIdx] = useState(0);
-  const totalLength = PROJECT_LIST.length;
-
-  const slidePrev = () => {
-    setIdx(prev => (prev === 0 ? totalLength - 1 : prev - 1));
-  };
-
-  const slideNext = () => {
-    setIdx(prev => (prev === totalLength - 1 ? 0 : prev + 1));
-  };
-
   return (
-    <div className={`relative text-base/8`}>
-      <div className={`relative overflow-hidden`}>
-        <div
-          className={`
-           flex flex-row transition-transform duration-500 ease-in-out`}
-          style={{ transform: `translateX(-${idx * 100}%)` }}
-        >
-          {PROJECT_LIST.map(v => {
-            return <ProjectComp key={`project-${v.name}`} targetProject={v} />;
-          })}
-        </div>
-      </div>
-      <button
-        className="absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-0 p-2 px-4 border-2 border-white bg-main-light-black text-white rounded-full"
-        onClick={slidePrev}
-      >
-        {"<"}
-      </button>
-      <button
-        className="absolute top-1/2 -translate-y-1/2 right-0 translate-x-1/2 p-2 px-4 border-2 border-white bg-main-light-black text-white rounded-full"
-        onClick={slideNext}
-      >
-        {">"}
-      </button>
-    </div>
+    <Carousel targetList={PROJECT_LIST}>
+      {PROJECT_LIST.map(v => {
+        return <ProjectComp key={`project-${v.name}`} targetProject={v} />;
+      })}
+    </Carousel>
   );
 }
