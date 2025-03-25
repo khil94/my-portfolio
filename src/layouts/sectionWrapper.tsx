14dfa6a6bda6b38c6ec92a7d4ce8aa0f3eac9e12
khil94 <zidru94@gmail.com>
2025-03-26 01:26:22 +0900

    미리 정의해둔 영역을 자식컴포넌트가 벗어나는 현상 수정
    자세한 것은 블로그 글에 쓸 예정

--- src/layouts/sectionWrapper.tsx
+++ src/layouts/sectionWrapper.tsx
@@ -10,10 +10,10 @@ export default function SectionWrapper({ type = "left", title, children }: Props
   return (
     <div
       id={title}
-      className={`w-full flex ${type === "left" ? "flex-row" : "flex-row-reverse"} items-center justify-center `}
+      className={`w-full flex ${type === "left" ? "flex-row" : "flex-row-reverse"} items-center`}
     >
       <h1 className={`min-w-60 text-2xl font-bold text-center`}>{title}</h1>
-      <div className={`p-12 flex text-base/7 `}>{children}</div>
+      <div className={`min-w-0 p-12 text-base/7`}>{children}</div>
     </div>
   );
 }
