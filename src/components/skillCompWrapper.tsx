936e104c7e968f6434c65b5c9c233eb75c9ff61e
khil94 <zidru94@gmail.com>
2025-03-26 04:04:52 +0900

    skillCompWrapper가 DivProps도 받을 수 있게 수정

--- src/components/skillCompWrapper.tsx
+++ src/components/skillCompWrapper.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from "react";
+import { HTMLProps, ReactNode } from "react";
 
-interface Props {
+interface Props extends HTMLProps<HTMLDivElement> {
   children: ReactNode;
   title: string;
 }
 
-export default function SkillCompWrapper({ title, children }: Props) {
+export default function SkillCompWrapper({ title, children, ...rest }: Props) {
+  const { className } = rest;
   return (
     <div
-      className={
-        "relative w-full flex flex-row flex-wrap p-6 rounded-xl border-2 border-white gap-6"
-      }
+      {...rest}
+      className={`relative w-full flex flex-row flex-wrap p-6 rounded-xl border-2 border-white gap-6 ${className}`}
     >
       <p className={"absolute top-0 -translate-y-1/2 bg-main-light-black p-2"}>{title}</p>
       {children}
