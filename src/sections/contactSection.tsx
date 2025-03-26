441de09c50ebc51c2a213784d056adc1dcebf851
khil94 <zidru94@gmail.com>
2025-03-27 00:39:42 +0900

    contactSection.tsx 내용 기입

--- src/sections/contactSection.tsx
+++ src/sections/contactSection.tsx
@@ -1,7 +1,35 @@
+import SectionWrapper from "../layouts/sectionWrapper.tsx";
+import Github from "../assets/github.svg?react";
+import Mail from "../assets/email.svg?react";
+import Blog from "../assets/blog.svg?react";
+
 export default function ContactSection() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Contact</h1>
-    </div>
+    <SectionWrapper title={"Contact"} type={"left"}>
+      <div className={`flex flex-col gap-8`}>
+        <a
+          className={"flex flex-row gap-8 items-center"}
+          href={"https://github.com/khil94"}
+          target={"_blank"}
+        >
+          <Github width={40} height={40} stroke={"white"} fill={"white"} />
+          <p>https://github.com/khil94</p>
+        </a>
+        <div className={"flex flex-row gap-8 items-center"}>
+          <a href={"mailto:khilkhil@naver.com"} target={"_blank"}>
+            <Mail width={40} height={40} stroke={"white"} fill={"white"} />
+          </a>
+          <p>khilkhil@naver.com</p>
+        </div>
+        <a
+          className={"flex flex-row gap-8 items-center"}
+          href={"https://zidru-blog.vercel.app/"}
+          target={"_blank"}
+        >
+          <Blog width={40} height={40} stroke={"white"} fill={"white"} />
+          <p>https://zidru-blog.vercel.app/</p>
+        </a>
+      </div>
+    </SectionWrapper>
   );
 }
