57f50757f201670777ea624bdddc40e58c6ca143
khil94 <zidru94@gmail.com>
2025-03-27 02:27:59 +0900

    약간의 스타일링(섹션 height 크기)

--- src/layouts/sectionWrapper.tsx
+++ src/layouts/sectionWrapper.tsx
@@ -10,10 +10,10 @@ export default function SectionWrapper({ type = "left", title, children }: Props
   return (
     <div
       id={title}
-      className={`w-full flex ${type === "left" ? "flex-row" : "flex-row-reverse"} items-center`}
+      className={`w-full min-h-[40vh] flex ${type === "left" ? "flex-row" : "flex-row-reverse"} items-center`}
     >
       <h1 className={`min-w-60 text-2xl font-bold text-center`}>{title}</h1>
-      <div className={`min-w-0 p-12 text-base/7`}>{children}</div>
+      <div className={`min-w-0 h-full p-12 text-base/7`}>{children}</div>
     </div>
   );
 }
--- src/sections/titleSection.tsx
+++ src/sections/titleSection.tsx
@@ -2,14 +2,14 @@ import { TITLE_PRODUCTS } from "../constants/title.ts";
 
 export default function TitleSection() {
   return (
-    <div className="w-full flex flex-col justify-center gap-10 items-center">
+    <div className="w-full h-[60vh] flex flex-col justify-center gap-10 items-center">
       <div className="w-fit h-[30vh]  flex flex-col text-left justify-around text-6xl font-bold p-10">
         {TITLE_PRODUCTS.main_title.map((v, i) => {
           return <p key={`main_title_${i}`}>{v}</p>;
         })}
       </div>
       <div className="">
-        <p>{TITLE_PRODUCTS.sub_title}</p>
+        <p className={"text-xl"}>{TITLE_PRODUCTS.sub_title}</p>
       </div>
     </div>
   );
